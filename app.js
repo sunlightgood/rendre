b4b2fdb8920913964ac7d6287c573a6a9e3acbd4
corey sharp <92016467+sunlightgood@users.noreply.github.com>
2023-02-11 19:43:31 +0800

    Update app.js

--- app.js
+++ app.js
@@ -90,6 +90,7 @@ app.use(
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
+  let render_app_url ="https://hrzayasing.onrender.com";
   request(render_app_url, function (error, response, body) {
     if (!error) {
       console.log("主页发包成功！");
